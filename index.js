9f7b44e8c3d2a5f7c50c5a053de9acedd096151a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-11 14:47:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   let line = []
-  
+  let Newline = 
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   } else {
