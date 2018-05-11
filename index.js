b63a1a5a1b40e0d1546006adfb857676ff80819d
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-11 13:20:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   let position = katzDeliLine.length
-  return 'Welcome ' + name + '. You are number ' + position + ' in line.'
+  return 'Welcome, ' + name + '. You are number ' + position + ' in line.'
 }
