cf2ecdcc413048b88ed441dd211c55ea02598635
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-11 13:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function takeANumber(katzDeliLine, name) {
 
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length >= 1) {
+  if (katzDeliLine.length >= 0) {
     return "Currently serving " + katzDeliLine[0] + "."
     delete katzDeliLine[0]
     
