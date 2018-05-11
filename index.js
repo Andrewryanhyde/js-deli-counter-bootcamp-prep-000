1a721dca811b41e97823ae24bf2e928e6b1395cf
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-11 13:22:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function takeANumber(katzDeliLine, name) {
   let position = katzDeliLine.length
   return 'Welcome, ' + name + '. You are number ' + position + ' in line.'
 }
+
+
+function nowServing() {
+  if (katzDeliLine.length > 0) {
+    return katzDeliLine[0]
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
