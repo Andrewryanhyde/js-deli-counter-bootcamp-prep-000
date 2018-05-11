e722ad8531932c53bd6f14c70be8595d12dc9ec8
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-05-11 13:37:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function takeANumber(katzDeliLine, name) {
 
 
 function nowServing(katzDeliLine) {
-  if (katzDeliLine.length >= 0) {
+  if (katzDeliLine.length >= 1) {
     return "Currently serving " + katzDeliLine[0] + "."
-    delete katzDeliLine[0]
+    katzDeliLine.shift()
     
   } else {
     return "There is nobody waiting to be served!"
